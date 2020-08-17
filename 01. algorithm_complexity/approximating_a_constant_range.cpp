d34eae4bc7c5def9ec0376e8e8d34d7e4a767151
le van linh <levanlinh1995@gmail.com>
2020-08-17 15:06:58 +0700

    update

---
+++ 01. algorithm_complexity/approximating_a_constant_range.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMinIndex(const vector<int> &arr, const int &i, const int &j) {
+    if (arr[j] > arr[i]) {
+        return i;
+    }
+
+    return j;
+}
+
+int findMaxIndex(const vector<int> &arr, const int &i, const int &j) {
+    if (arr[i] > arr[j]) {
+        return i;
+    }
+
+    return j;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+
+    int N, A;
+    vector<int> arr;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A;
+        arr.push_back(A);
+    }
+    
+
+    int len = 0;
+    int i = 0;
+    int j = 1;
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    while (j < N && i < N)
+    {
+        minIndex = findMinIndex(arr, minIndex, j);
+        maxIndex = findMaxIndex(arr, maxIndex, j);
+
+        if (abs(arr[minIndex] - arr[maxIndex]) <= 1) {
+            len = max(len, j - i + 1);
+            j++;
+        } else {
+            if (maxIndex == j) {
+                minIndex = i = min(minIndex + 1, N - 1);
+            }
+
+            if (minIndex == j) {
+                maxIndex = i = min(maxIndex + 1, N - 1);
+            }
+        }
+    }
+
+    cout << len;
+
+    return 0;
+}
---
+++ 01. algorithm_complexity/wrath.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    int n, L;
+    vector<int> arr;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> L;
+        arr.push_back(L);
+    }
+
+    int j = n - 1;
+    int latest_kill_index = 0;
+    int count = 0;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        j = min(i, j);
+
+        latest_kill_index = max(0, i - arr[i]);
+
+        if (latest_kill_index < j) {
+            count += (j - latest_kill_index);
+            j = latest_kill_index;
+        }
+    }
+
+    cout << n - count;
+    
+
+    return 0;
+}
